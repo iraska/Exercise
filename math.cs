214ab0c650f24257c73400348c5a5a67babd16b7
İrem Turna <105501017+iraska@users.noreply.github.com>
2024-05-02 16:52:09 +0300

    Create math.cs

---
+++ math.cs
@@ -0,0 +1,11 @@
+/* Code challenge: Implement a method of the Math class that returns the larger of two numbers
+    int firstValue = 500;
+    int secondValue = 600;
+    int largerValue;
+    Console.WriteLine(largerValue);*/
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);  // 600
