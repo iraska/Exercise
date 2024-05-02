b67458a74d701f97294aacfffe8248a0d7437c43
İrem Turna <105501017+iraska@users.noreply.github.com>
2024-05-02 22:22:16 +0300

    Create Readability.cs

---
+++ Readability.cs
@@ -0,0 +1,34 @@
+/* Code challenge - apply style guidelines to improve readability 
+    string str = "The quick brown fox jumps over the lazy dog.";
+    // convert the message into a char array
+    char[] charMessage = str.ToCharArray();
+    // Reverse the chars
+    Array.Reverse(charMessage);
+    int x = 0;
+    // count the o's
+    foreach (char i in charMessage) { if (i == 'o') { x++; } }
+    // convert it back to a string
+    string new_message = new String(charMessage);
+    // print it out
+    Console.WriteLine(new_message);
+    Console.WriteLine($"'o' appears {x} times."); */
+
+// The high-level purpose of this code is to reverse a string and count the number of times a particular character appears.
+
+string initialMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = initialMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char i in message) 
+{ 
+    if (i == 'o') 
+        letterCount++; 
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
